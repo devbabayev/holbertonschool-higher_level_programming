1541c8415b450f6e1a93cea9ff740d6a3a3c6adb
root <thebabayev.dev@gmail.com>
2025-12-27 14:29:17 +0000

    strs merge

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # str line to 
 str = 'Holberton School'
-print(f"{str}{str[:9]} {str}{str[:9]} {str[10:]} /n {str[:9]}")
+print(f"{str}{str[:9]} {str}{str[:9]} {str[10:]} {/n} {str[:9]}")
