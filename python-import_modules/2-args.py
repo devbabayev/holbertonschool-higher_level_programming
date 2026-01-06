f5607906c542c9d33a916b17185772c6b1c55bee
root <thebabayev.dev@gmail.com>
2026-01-06 12:51:57 +0000

    fix 7

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         print("{}: {}".format(count, sys.argv[1]))
     else:
         print("{} arguments".format(count))
-        for i in range(1, count):
+        for i in range(0, count):
             print("{}: {}".format(count, sys.argv[i]))
