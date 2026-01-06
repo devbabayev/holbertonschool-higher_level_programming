bf07091aed06dd81c2e815daeba7c94df95856d7
root <thebabayev.dev@gmail.com>
2026-01-06 12:32:04 +0000

    make a script dynamic

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    count = len(argv) - 1
+
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+        print("{}: {}".format(count, sys.argv[1]))
+    else:
+        for i in range(1, count):
+            print("{}: {}".format(count, sys.argv[i]))
