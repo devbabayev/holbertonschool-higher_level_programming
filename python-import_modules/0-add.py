91650387d2001c8e1b30c509444df82fc71d6487
root <thebabayev.dev@gmail.com>
2026-01-06 11:17:23 +0000

    import a simple function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    print("{} + {} = {}".format(a, b, a+b))
-add(1,2)
+from add_0 import add
+a = 1
+b = 2
+if __name__== "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b))
