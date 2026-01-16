54e03953cf05f742941f73c55d8721cdf1b80768
root <thebabayev.dev@gmail.com>
2026-01-16 13:06:37 +0000

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     list = my_list
     if idx < 0:
         return list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return list
     list[idx] = element
     return list
