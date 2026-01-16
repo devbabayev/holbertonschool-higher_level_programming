b1661e64678241b6efde0764095d0ce192f208d4
root <thebabayev.dev@gmail.com>
2026-01-16 10:57:50 +0000

    Safe printing of a integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if '0' < value < '9':
-            print("{:d}".format(value))
-            return True
-    except TypeError:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
         print("{} is not an integer".format(value))
         return False
