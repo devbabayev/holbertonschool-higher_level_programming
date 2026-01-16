dfcec0c03d47566bdc527657fc13016ad721ede4
root <thebabayev.dev@gmail.com>
2026-01-16 12:06:51 +0000

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside result {}".format(a / b))
-    except TypeError:
+        return a / b
+    except ZeroDivisionError:
         return None
     finally:
-        print("{} / {} = {}".format(a / b))
+        try:
+            print("Inside result: {}".format(a / b))
+        except ZeroDivisionError:
+            print("Inside result: {}".format(None))
