06544eea8814b606fa0dd6d87b95cc7025258293
root <thebabayev.dev@gmail.com>
2026-01-16 11:50:53 +0000

    Inside result with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("{} / {} = {}".format(a, b, a / b))
+        print("Inside result {}".format(a / b))
     except TypeError:
         return None
     finally:
-        print("Inside result {}".format(a / b))
+        print("{} / {} = {}".format(a / b))
