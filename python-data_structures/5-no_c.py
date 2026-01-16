4e1d3d507692ab60fd488b89fbc6644d774395df
root <thebabayev.dev@gmail.com>
2026-01-16 13:24:48 +0000

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,5 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i == 'c':
-            new_string += " "
-        else:
-            new_string += i
-    return new_string
+            del my_string[i]
+    return my_string
