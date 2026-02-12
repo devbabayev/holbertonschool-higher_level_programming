8e63a10a82ea3a6f25929096b72f35fb2734798b
root <thebabayev.dev@gmail.com>
2026-02-12 11:22:20 +0000

    Simple delete by key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+def simple_delete(a_dictionary, key=""):
+    a_dictionary.pop(key, None)
     return a_dictionary
